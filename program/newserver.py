3e6bb4e37c3be601f92ad97b6f00dacef60cb27e
shuairuan <rsow001@gmail.com>
2020-10-16 20:37:37 +0800

    Fixed

--- program/newserver.py
+++ program/newserver.py
@@ -5,7 +5,9 @@ def create(servername):
     print("Server name: " + servername)
     print("Creating server...")
     print(os.getcwd())
-    os.system('cd storage && mkdir ' + servername + ' && cd ' + servername + ' echo "{}" >> item.json')
+    os.system('cd storage && mkdir "' + servername + '"')
+    with open("storage/" + servername + "/item.json", 'w') as f:
+        json.dump({}, f)
     print("Reading server list...")
     with open("storage/map.json", "r") as cserver:
         map = json.load(cserver)
